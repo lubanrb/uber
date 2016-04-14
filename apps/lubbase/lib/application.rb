82519fd3d1990911e6e2206c5998bf8378eb1ca1
Chi Man Lei <chi.lei@laxino.com>
2016-04-15 00:27:07 +0800

    Created Luban profile for Uber environment

--- apps/lubbase/lib/application.rb
+++ apps/lubbase/lib/application.rb
@@ -1,4 +1,26 @@
 class Uber
   class Lubbase < Luban::Deployment::Application
+    class Builder < Luban::Deployment::Application::Builder
+      def luban_profile_file
+        @luban_profile_file ||= app_path.join('.luban_profile')
+      end
+
+      def luban_profile_template_file
+        @luban_profile_template_file ||= find_template_file("luban_profile.erb")
+      end
+
+      def build
+        super
+        create_luban_profile
+      end
+
+      protected
+
+      def create_luban_profile
+        upload_by_template(file_to_upload: luban_profile_file,
+                           template_file:  luban_profile_template_file,
+                           auto_revision: true)
+      end
+    end
   end
 end
