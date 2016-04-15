986f96bf78af7b79549a47529839fe55b5bc7fbc
Chi Man Lei <chi.lei@laxino.com>
2016-04-15 17:59:01 +0800

    Handled append/remove Luban profile in bashrc when setup/destroy Uber environment

--- apps/lubbase/lib/application.rb
+++ apps/lubbase/lib/application.rb
@@ -5,6 +5,14 @@ class Uber
         @luban_profile_file ||= app_path.join('.luban_profile')
       end
 
+      def bashrc_file
+        @bashrc_file ||= Pathname.new(user_home).join('.bashrc')
+      end
+
+      def source_command
+        @source_command ||= "source #{luban_profile_file} #{bashrc_file}"
+      end
+
       def luban_profile_template_file
         @luban_profile_template_file ||= find_template_file("luban_profile.erb")
       end
@@ -12,6 +20,12 @@ class Uber
       def build
         super
         create_luban_profile
+        update_bashrc
+      end
+
+      def destroy
+        cleanup_bashrc
+        super
       end
 
       protected
@@ -20,6 +34,19 @@ class Uber
         upload_by_template(file_to_upload: luban_profile_file,
                            template_file:  luban_profile_template_file,
                            auto_revision: true)
+      end
+
+      def update_bashrc
+        unless test(:grep, "-q -F '#{source_command}' #{bashrc_file}")
+          execute(:printf, "'#{source_command}' >> #{bashrc_file}")
+        end
+      end
+
+      def cleanup_bashrc
+        require 'tempfile'
+        tmp_file = Dir::Tmpname.make_tmpname(tmp_path.join(".bashrc").to_s, nil)
+        execute(:grep, "-v '#{source_command}' #{bashrc_file} > #{tmp_file}")
+        mv(tmp_file, bashrc_file)
       end
     end
   end
