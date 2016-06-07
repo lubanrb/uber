361c89d6e648b7e25f538d08449e8e0897511c63
Chi Man Lei <chi.lei@laxino.com>
2016-06-07 12:09:39 +0800

    monitor and unmonitor are no more common tasks

--- apps/lubmon/lib/application.rb
+++ apps/lubmon/lib/application.rb
@@ -1,11 +1,4 @@
 class Uber
   class Lubmon < Luban::Deployment::Application
-    protected
-
-    def setup_control_tasks
-      super
-      undef_task :monitor
-      undef_task :unmonitor
-    end
   end
 end
--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,2 @@
 class Uber < Luban::Deployment::Project
-  protected
-
-  def setup_control_tasks
-    super
-    undef_task :monitor
-    undef_task :unmonitor
-  end
 end
