c08016d8cd754e3023c886b8170c70b045db2ab5
Chi Man Lei <chi.lei@laxino.com>
2016-10-24 19:00:26 +0800

    Cleaned up the design and implementation of Lublog app parameters in a deployment project

--- apps/lublog/lib/application.rb
+++ apps/lublog/lib/application.rb
@@ -1,50 +1,25 @@
 class Uber
   class Lublog < Luban::Deployment::Application
-    module Parameters
-      extend Luban::Deployment::Parameters::Base
- 
-      parameter :central_archives_path
-
-      def central_archives_host; primary(:archive) end
-
-      def log_archiver_command(cmd = './log-archiver')
-        @log_archiver_command = "#{cmd} #{stage} #{local_archives_path} #{central_archives_host}:#{central_archives_path}"
-      end
-
-      def default_source_path
-        @default_source_path ||= config_finder[:application].base_path.join('app')
-      end
-
-      protected
-
-      def set_default_archives_parameters
-        set_default :central_archives_path, luban_root_path.join('central-archives')      
-      end
-    end
-
-    include Parameters
-
-    def setup_logrotate
-      cronjob schedule: "*/#{logrotate_interval} * * * *",
-              command: ->{ log_archiver_command },
-              output: "lublog.log"
-    end
+    parameter :central_archives_host
+    parameter :central_archives_path
 
     protected
 
-    def set_default_application_parameters
+    def set_parameters
       super
-      set_default_archives_parameters
-      set_default_source
+      set :central_archives_path, luban_root_path.join('central-archives')
     end
 
     def after_configure
       super
+      set :central_archives_host, primary(:archive)
       setup_logrotate
     end
 
-    class Crontab < Luban::Deployment::Application::Crontab
-      include Parameters
+    def setup_logrotate
+      cronjob schedule: "*/#{logrotate_interval} * * * *",
+              command: ->{ "./log-archiver #{control_file_path}" },
+              output: "lublog.log"
     end
   end
 end
