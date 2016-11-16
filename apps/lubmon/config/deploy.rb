46e4850f50369feee0454e632e85905717d4d231
Chi Man Lei <chi.lei@laxino.com>
2016-11-16 09:04:19 +0800

    Started Monit without activating envrc in cronjob for server reboot

--- apps/lubmon/config/deploy.rb
+++ apps/lubmon/config/deploy.rb
@@ -1,1 +1,6 @@
 # Deployment configuration for application Uber::Lubmon
+
+cronjob schedule: "@reboot",
+        command: ->{ "#{package_bin_path("monit").join("monit")}" + 
+                     " -c #{package_install_path("monit").join("etc", "monitrc")}" },
+        setup: [], output: nil
--- apps/lubmon/config/deploy/development.rb
+++ apps/lubmon/config/deploy/development.rb
@@ -3,6 +3,3 @@
 # Application servers
 role :app, `hostname -f`.chomp
 
-cronjob schedule: "@reboot",
-        command: ->{ package_bin_path("monit").join("monit") },
-        output: nil
