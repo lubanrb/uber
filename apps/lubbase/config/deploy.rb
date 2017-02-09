02b4985d38ca551fd1e38acfdb4f4f736701198f
Chi Man Lei <chi.lei@laxino.com>
2017-02-09 22:34:47 +0800

    Added docker support for Lubbase

--- apps/lubbase/config/deploy.rb
+++ apps/lubbase/config/deploy.rb
@@ -1,1 +1,5 @@
 # Deployment configuration for application Uber::Lubbase
+
+# Turn on docker support
+dockerize
+timezone 'Asia/Shanghai'
--- apps/lubbase/config/deploy/development.rb
+++ apps/lubbase/config/deploy/development.rb
@@ -1,4 +1,6 @@
 # Deployment configuration for development application Uber::Lubbase
 
+build_tag "1.0.0"
+
 # Application servers
 role :app, `hostname -f`.chomp
