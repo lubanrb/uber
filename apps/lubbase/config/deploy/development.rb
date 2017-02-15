d98a28f2807100a72766756698c08002857e2a3e
Chi Man Lei <chi.lei@laxino.com>
2017-02-15 09:02:33 +0800

    Minor enhancements:   * Added support for new enhancements from Luban 0.12.3     * Supported luban_uid to avoid permission issues on docker data volume     * Used TZ to setup docker image timezone during run-time instead of build-time   * Bump up build tag to 1.0.1

--- apps/lubbase/config/deploy/development.rb
+++ apps/lubbase/config/deploy/development.rb
@@ -1,6 +1,6 @@
 # Deployment configuration for development application Uber::Lubbase
 
-build_tag "1.0.0"
+build_tag "1.0.1"
 
 # Application servers
-role :app, `hostname -f`.chomp
+role :app, `hostname -f`.chomp, docker_unix_socket: "/var/run/docker.sock"
