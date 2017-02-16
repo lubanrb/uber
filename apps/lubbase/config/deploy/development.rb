141435f4ec6439b4d77684080537ff5f1493fe5b
Chi Man Lei <chi.lei@laxino.com>
2017-02-16 14:30:10 +0800

    Bump up build tag to 1.0.2

--- apps/lubbase/config/deploy/development.rb
+++ apps/lubbase/config/deploy/development.rb
@@ -1,6 +1,6 @@
 # Deployment configuration for development application Uber::Lubbase
 
-build_tag "1.0.1"
+build_tag "1.0.2"
 
 # Application servers
 role :app, `hostname -f`.chomp, docker_unix_socket: "/var/run/docker.sock"
