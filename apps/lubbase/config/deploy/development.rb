d656e8328f8f7071f823b3a677ff7cfd301dc9d3
Chi Man Lei <chi.lei@laxino.com>
2017-02-23 17:54:06 +0800

    * Upgraded Git in development to version 2.11.1   * Upgraded Ruby in development to version 2.4.0   * Bump up build tag to 1.0.4

--- apps/lubbase/config/deploy/development.rb
+++ apps/lubbase/config/deploy/development.rb
@@ -1,6 +1,6 @@
 # Deployment configuration for development application Uber::Lubbase
 
-build_tag "1.0.2"
+build_tag "1.0.4"
 
 # Application servers
 role :app, `hostname -f`.chomp, docker_unix_socket: "/var/run/docker.sock"
@@ -8,5 +8,5 @@ role :app, `hostname -f`.chomp, docker_unix_socket: "/var/run/docker.sock"
 base_packages [ "make", "gcc", "which", "rsync", "openssh-clients", "zlib-devel", "perl-devel" ]
 
 package "curl", version: "7.52.1", current: true
-package "git", version: "2.11.0", current: true
-package "ruby", version: "2.3.3", current: true
+package "git", version: "2.11.1", current: true
+package "ruby", version: "2.4.0", current: true
