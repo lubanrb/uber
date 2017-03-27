2142488bb104c48ea010344007dec1e60caee421
Chi Man Lei <chi.lei@laxino.com>
2017-03-27 10:10:35 +0800

    * Added dev package dependencies for nodejs, mysql & sqlite   * Upgraded Curl to version 7.53.1   * Upgraded Git to 2.12.0   * Bump up build tag to 1.0.5

--- apps/lubbase/config/deploy/development.rb
+++ apps/lubbase/config/deploy/development.rb
@@ -1,12 +1,14 @@
 # Deployment configuration for development application Uber::Lubbase
 
-build_tag "1.0.4"
+build_tag "1.0.5"
 
 # Application servers
 role :app, `hostname -f`.chomp, docker_unix_socket: "/var/run/docker.sock"
 
-base_packages [ "make", "gcc", "which", "rsync", "openssh-clients", "zlib-devel", "perl-devel" ]
+base_packages [ "make", "gcc", "which", "rsync", "openssh-clients", 
+                "zlib-devel", "perl-devel",
+                "epel-release", "nodejs", "sqlite-devel", "mysql-devel" ]
 
-package "curl", version: "7.52.1", current: true
-package "git", version: "2.11.1", current: true
+package "curl", version: "7.53.1", current: true
+package "git", version: "2.12.0", current: true
 package "ruby", version: "2.4.0", current: true
