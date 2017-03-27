8146131a1ff712fbe31bd0a1f801bef00fc82990
Chi Man Lei <chi.lei@laxino.com>
2017-03-28 00:20:53 +0800

    Installed epel-release package repo before the installation of nodejs

--- apps/lubbase/config/deploy/development.rb
+++ apps/lubbase/config/deploy/development.rb
@@ -5,9 +5,9 @@ build_tag "1.0.5"
 # Application servers
 role :app, `hostname -f`.chomp, docker_unix_socket: "/var/run/docker.sock"
 
-base_packages [ "make", "gcc", "which", "rsync", "openssh-clients", 
+base_packages [ "make", "gcc", "gcc-c++", "which", "rsync", "openssh-clients", 
                 "zlib-devel", "perl-devel",
-                "epel-release", "nodejs", "sqlite-devel", "mysql-devel" ]
+                "nodejs", "sqlite-devel", "mysql-devel" ]
 
 package "curl", version: "7.53.1", current: true
 package "git", version: "2.12.0", current: true
--- apps/lubbase/config/deploy/standard.rb
+++ apps/lubbase/config/deploy/standard.rb
@@ -7,7 +7,7 @@ role :app, `hostname -f`.chomp, docker_unix_socket: "/var/run/docker.sock"
 
 base_packages [ "make", "gcc", "which", "rsync", "openssh-clients", 
                 "zlib-devel", "perl-devel",
-                "epel-release", "nodejs", "sqlite-devel", "mysql-devel" ]
+                "nodejs", "sqlite-devel", "mysql-devel" ]
 
 package "curl", version: "7.53.1", current: true
 package "git", version: "2.12.0", current: true
